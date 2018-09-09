951b79215d1755b3fa430d1bba931a018e9275a2
Usman Nazir <G_4_Genious@hotmail.com>
2018-09-09 16:06:41 +0500

    adflkajdsf

--- JulyMVCORMConfig/Startup.cs
+++ JulyMVCORMConfig/Startup.cs
@@ -51,6 +51,10 @@ namespace JulyMVCORMConfig
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
+
+
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
